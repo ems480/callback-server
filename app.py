8fdc0731cff245908b009b7b60346ddb842825c1
ems480 <116377770+ems480@users.noreply.github.com>
2025-10-12 18:47:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -366,6 +366,7 @@ def request_loan():
         cur.execute(
                 "SELECT * FROM estack_transactions WHERE name_of_transaction LIKE ? AND status = 'COMPLETED'",
                 (f"%{investment_id}%",)
+            )
         row = cur.fetchone()
 
         if not row:
@@ -2280,6 +2281,7 @@ def get_pending_loans():
 #         init_db()
 #     port = int(os.environ.get("PORT", 5000))
 #     app.run(host="0.0.0.0", port=port)
+
 
 
 
