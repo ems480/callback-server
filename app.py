2437f054bf139864f18f537331320ba4419cba91
ems480 <116377770+ems480@users.noreply.github.com>
2025-10-18 02:30:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from dotenv import load_dotenv
 load_dotenv()
 
-# 3
 from flask import Flask, request, jsonify, g
 import os, logging, sqlite3, json, requests, uuid
 from datetime import datetime
@@ -2544,3 +2543,4 @@ def get_pending_loans():
 #         init_db()
 #     port = int(os.environ.get("PORT", 5000))
 #     app.run(host="0.0.0.0", port=port)
+
